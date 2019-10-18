3b04cf79686739591c2f891500d8b6776e99a960
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-18 11:50:36 -0400

    API, dependency array, and mapping through data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios"; 
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [list, setList] = useState([]);
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
+     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const recieveList = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character/") 
+        .then(response => {
+          const list = response.data.results;
+          console.log("Character Data", response);
+          setList(list);
+        })
+        .catch(error => {
+          console.log("Sorry, an error has occured", err);
+        });
+    };
+    recieveList();
   }, []);
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {list.map(character => {
+        return (
+
+        )
+      })}
     </section>
   );
 }
+
+export default CharacterList;
